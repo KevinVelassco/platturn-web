085623d77c316550bf42278ebc36babd2f97cc52
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2020-03-04 03:52:02 +0000

    feat(web): sidebar

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
       <div class="container">
-        <router-link to="/" class="navbar-brand">
+        <a href="#" @click.prevent="showSideBar">
           <img
             src="./assets/logo.png"
             height="30px"
             width="30px"
             alt="Platturn"
           />
-        </router-link>
+        </a>
         <button
           class="navbar-toggler"
           type="button"
@@ -77,6 +77,20 @@
       </div>
     </nav>
 
+    <div
+      v-if="currentUser && renderSideBar"
+      id="mySidebar"
+      class="sidebar bg-dark"
+    >
+      <a href="#" class="closebtn" v-on:click="closeSideBer">
+        ×
+      </a>
+      <a href="#">About</a>
+      <a href="#">Services</a>
+      <a href="#">Clients</a>
+      <a href="#">Contact</a>
+    </div>
+
     <div class="container">
       <router-view />
     </div>
@@ -85,6 +99,12 @@
 
 <script>
 export default {
+  name: "App",
+  data() {
+    return {
+      renderSideBar: false
+    };
+  },
   computed: {
     currentUser() {
       return this.$store.state.auth.user;
@@ -108,7 +128,83 @@ export default {
     logOut() {
       this.$store.dispatch("auth/logout");
       this.$router.push("/login");
+    },
+    showSideBar() {
+      if (!this.currentUser) {
+        const {
+          currentRoute: { path }
+        } = this.$router;
+
+        if (path !== "/") this.$router.push("/");
+        return;
+      }
+      this.renderSideBar = true;
+    },
+    closeSideBer() {
+      this.renderSideBar = false;
     }
   }
 };
 </script>
+<style scoped>
+.sidebar {
+  height: 100%;
+  width: 177px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidebar a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidebar a:hover {
+  color: #f1f1f1;
+}
+
+.sidebar .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+.openbtn {
+  font-size: 20px;
+  cursor: pointer;
+  background-color: #111;
+  color: white;
+  padding: 10px 15px;
+  border: none;
+}
+
+.openbtn:hover {
+  background-color: #444;
+}
+
+#main {
+  transition: margin-left 0.5s;
+  padding: 16px;
+}
+
+/* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
+@media screen and (max-height: 450px) {
+  .sidebar {
+    padding-top: 15px;
+  }
+  .sidebar a {
+    font-size: 18px;
+  }
+}
+</style>
