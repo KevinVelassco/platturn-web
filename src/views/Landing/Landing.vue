a13f71873d64469da96537bffb1e5962a9d21521
Kevin Andres velasco <kevinvelassco@gmail.com>
2020-04-03 11:54:40 -0500

    feat(web): adjustments to the landing page

--- src/views/Landing/Landing.vue
+++ src/views/Landing/Landing.vue
@@ -8,48 +8,7 @@ export default {
     }
   },
   data() {
-    return {
-      plans: [
-        {
-          code: 1,
-          description: "GRATIS",
-          priceCode: "",
-          priceValue: "Gratis",
-          paymentType: "",
-          color: "red",
-          features: ["Consultar Clientes", "Liquidar Nomina"]
-        },
-        {
-          code: 2,
-          description: "ESTANDAR",
-          priceCode: "CO",
-          priceValue: "$6.000",
-          paymentType: "/ mes",
-          color: "#ffa000",
-          features: [
-            "Consultar Clientes",
-            "Crear Clientes",
-            "Editar Clientes",
-            "Liquidar Nomina"
-          ]
-        },
-        {
-          code: 3,
-          description: "PREMIUM",
-          priceCode: "CO",
-          priceValue: "$12.000",
-          paymentType: "/ mes",
-          color: "#0b00ff",
-          features: [
-            "Consultar Clientes",
-            "Crear Clientes",
-            "Editar Clientes",
-            "Eliminar Clientes",
-            "Liquidar Nomina"
-          ]
-        }
-      ]
-    };
+    return {};
   },
   mounted() {
     if (this.currentUser) {
